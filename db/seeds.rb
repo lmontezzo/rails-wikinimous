89326ba7b3b2d3410b339f9c42328e00e5b2f140
Luciano Montezzo <lmontezzo@gmail.com>
2017-04-11 22:42:01 -0300

    wikinimous done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  articles = Article.create ([{title: Faker::LordOfTheRings.character, content: Faker::Lorem.paragraph(2) }])
+end
